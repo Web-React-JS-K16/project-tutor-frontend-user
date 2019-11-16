484cdac87a74e1389a6cdf5bc7ff75d39b72b200
TranTLy <tranly237@gmail.com>
2019-11-16 17:33:07 +0700

    refactor architecture of folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import LoginPage from './components/LoginPage/LoginPage.component'
+import 'antd/dist/antd.css'
 
 const App = () => {
-  return <div>This is app</div>;
-};
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/login" component={LoginPage} />
+      </Switch>
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/LoginPage/LoginPage.component.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prop-types */
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { Form, Icon, Input, Button } from 'antd'
+import './LoginPage.style.scss'
+
+const LoginPage = ({ form }) => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+
+  const { getFieldDecorator } = form
+
+  return (
+    <div className="login-page">
+      <h1 className="login-page__title">Đăng nhập</h1>
+      <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [{ required: true, message: 'Vui lòng nhập email!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Email"
+              type="mail"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [
+              {
+                required: true,
+                message: 'Vui lòng nhập mật khẩu!',
+              },
+            ],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Mật khẩu"
+            />
+          )}
+        </Form.Item>
+        <div className="login-form__bottom">
+          <div>
+            <a className="login-form-forgot" href="">
+              Quên mật khẩu
+            </a>
+          </div>
+          <Button type="primary" htmlType="submit" className="login-form-button btn-login">
+            Đăng nhập
+          </Button>
+          <div className="register">
+            Hoặc <a href="">Đăng ký</a>
+          </div>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
+LoginPage.propTypes = {}
+
+export default Form.create({ name: 'LoginForm' })(LoginPage)

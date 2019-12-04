03f6607ac41cf8807a106af253b32a0168320397
Tran Ly <tranly237@gmail.com>
2019-12-04 10:42:25 +0700

    fix link

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,3 +1,10 @@
-const apiUrl = 'http://localhost:4500'
+let hostURL
+if (process.env.NODE_ENV === 'production') {
+  hostURL = 'https://tutor-back-end-user.herokuapp.com'
+} else {
+  hostURL = 'http://localhost:4500'
+}
+
+const apiUrl = hostURL
 
 export default apiUrl

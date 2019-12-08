0eaa315656d70d00d4bd1b1b9ce1d2151fc55455
Tran Ly <tranly237@gmail.com>
2019-12-09 00:29:59 +0700

    fix link

--- src/services/api-url.js
+++ src/services/api-url.js
@@ -1,4 +1,8 @@
-const apiUrl = 'https://tutor-back-end-user.herokuapp.com'
+const apiUrl =
+  process.env.NODE_ENV === 'production'
+    ? 'https://tutor-back-end-user.herokuapp.com'
+    : 'http://localhost:4500'
+
 // const apiUrl = 'http://localhost:4500'
 
 export default apiUrl
